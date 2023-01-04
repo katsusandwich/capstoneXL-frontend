07cb95b37566cba4089cf68ac7b77df9409674e7
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2023-01-04 16:57:34 +0800

    test works but have to change interxn w input box

--- src/Page/TestPage.js
+++ src/Page/TestPage.js
@@ -8,7 +8,7 @@ import {
   TextInput,
   Stack,
 } from "@mantine/core";
-import "../CSS/listMakingPage.css";
+import "../CSS/testPage.css";
 import { useNavigate } from "react-router";
 import { BACKEND_URL } from "../constants";
 import { useSelectedWordlistIdContext } from "../Context/SelectedWordlistIdContext";
@@ -27,6 +27,55 @@ const TestPage = () => {
     setSelectedWordlistName,
   } = useSelectedWordlistNameContext();
 
+  //text input box
+  // const [answerEntered, setAnswerEntered] = useState("");
+  const answerEntered = "hello";
+  const key = "kun_readings";
+
+  const kanji = {
+    kanji: "蛍",
+    meanings: ["lightning-bug", "firefly"],
+    kun_readings: ["ほたる"],
+    on_readings: ["ケイ"],
+    name_readings: [],
+  };
+
+  //handle getTest
+  const handleGetTest = (event) => {
+    event.preventDefault();
+    // axios
+    //   .get(`https://kanjiapi.dev/v1/kanji/${wordEntered}`)
+    //   .then((res) => res.data)
+    //   .then((res) => {
+    //     console.log(`This is res of handleGetKanji ${JSON.stringify(res)}`);
+    //     setWordToBeAdded(res);
+    //     // console.log(`wordToBeAdded is ${JSON.stringify(wordToBeAdded)}`);　this is blank because asynchronous
+    //   });
+  };
+
+  //testing function
+  const testFunction = (answerEntered, key, kanji) => {
+    // If the key exists in the object
+    if (key in kanji) {
+      // Check if the value of the key is an array
+      if (Array.isArray(kanji[key])) {
+        // Return true if the search string is a full and complete match for any element in the array
+        return kanji[key].some((val) => val === answerEntered);
+      } else {
+        // Return true if the search string is a full and complete match for the value of the key
+        return answerEntered === kanji[key];
+      }
+    }
+    // Otherwise, return false
+    return false;
+  };
+
+  //useEffect
+
+  useEffect(() => {
+    console.log(testFunction(answerEntered, key, kanji));
+  }, []);
+
   return (
     <Stack
       align="flex-start"
@@ -40,7 +89,28 @@ const TestPage = () => {
         <Container className="testHeader" fluid>
           Test in Progress: {selectedWordlistName}
         </Container>
-        <TextInput placeholder="Your answer here" label="What is this?" />
+        <Container>
+          <form>
+            <input
+              type="text"
+              value={answerEntered}
+              onChange={(e) => {
+                console.log(`answerEntered in input is ${e.target.value}`);
+                // setAnswerEntered(e.target.value);
+              }}
+            />
+            <button
+              type="button"
+              onClick={(event) => {
+                handleGetTest(event);
+              }}
+              className="testButton"
+              value="Check answer"
+            >
+              Check answer
+            </button>
+          </form>
+        </Container>
       </div>
     </Stack>
   );
